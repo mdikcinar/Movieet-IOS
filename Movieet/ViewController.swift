fed688a5aada2cb0f61b8fdb886d3e17c75a342f
Mustafa Ali Dikçinar <mustafaalidikcinar@hotmail.com>
2023-07-01 18:05:38 +0300

    Initial Commit

---
+++ Movieet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movieet
+//
+//  Created by Mustafa Ali Dikcinar on 1.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
