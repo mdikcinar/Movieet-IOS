ec9f6e5c35e094b5cd5aae7673087f73b8e24144
Mustafa Ali Dikçinar <mustafaalidikcinar@hotmail.com>
2023-07-01 18:25:24 +0300

    initial commit

---
+++ Movieet/Features/Dashboard/MVTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MVTabViewController.swift
+//  Movieet
+//
+//  Created by Mustafa Ali Dikcinar on 1.07.2023.
+//
+
+import UIKit
+
+class MVTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
